9f9596dc7ca304eced0558637848596112275220
Brian Sunter <bsunter@gmail.com>
2016-07-21 15:21:49 -0400

    Initial Commit

---
+++ Flickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by brian Sunter on 7/21/16.
+//  Copyright © 2016 Brian Sunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
